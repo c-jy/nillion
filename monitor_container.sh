8275c257fa38eda5bb46a0d45df387d4b4b6275e
cjy <jiayong_ch@126.com>
2025-02-13 17:25:51 +0800

    test

--- monitor_container.sh
+++ monitor_container.sh
@@ -46,4 +46,25 @@ function send_msg() {
     curl -X POST -H "Content-Type: application/json" -d "{\"msg_type\":\"text\",\"content\":{\"text\":\"nillion $1\"}}" https://open.feishu.cn/open-apis/bot/v2/hook/99c1aa52-0568-420a-b337-4946fa4814d7
 }
 
+function killProcess() {
+    # 查询指定名称的进程并杀掉
+    PROCESS_NAME="monitor_container"
+    
+    # 查询进程ID
+    PID=$(pgrep "${PROCESS_NAME}")
+    
+    # 检查进程是否存在
+    if [ ! -z "$PID" ]; then
+        # 杀掉进程
+        kill $PID
+        # 检查是否杀掉
+        if kill -0 $PID > /dev/null 2>&1; then
+            echo "进程 $PID 未能被杀掉。"
+        else
+            echo "进程 $PID 已被杀掉。"
+        fi
+    else
+        echo "没有找到进程 $PROCESS_NAME。"
+    fi
+}
 main 
